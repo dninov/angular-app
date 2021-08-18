b0debb95ad1c168fdecb300bc0ffa4eaafdc4d55
Delyan <dninov@gmail.com>
2021-08-18 15:58:56 +0300

    Auth Reducer

---
+++ src/app/app.reducer.ts
@@ -0,0 +1,13 @@
+import { ActionReducerMap, createFeature, createFeatureSelector, createSelector } from "@ngrx/store";
+
+import * as fromAuth from './auth/auth.reducer'; 
+
+export interface State {
+    auth: fromAuth.State;
+}
+
+export const reducers: ActionReducerMap<State, any> = {
+    auth: fromAuth.authReducer
+}
+
+export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
